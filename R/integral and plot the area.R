700e0326f5d91c9f0385b48074ef4790974e8ea6
MaryAnne0725 <102033163+MaryAnne0725@users.noreply.github.com>
2022-07-23 12:53:22 +0200

    improper and undefined extremes of the integral

--- R/integral and plot the area.R
+++ R/integral and plot the area.R
@@ -1,6 +1,7 @@
 #' Compute defined integral and draw the area that is referred to
 #' 
-#' The goal of this function is to compute defined integral and to show the corresponding area.
+#' The goal of this function is to compute defined integral and to show the corresponding area. 
+#' Note that this function can be adapted also for indefinite integrals (integrals between -Inf and Inf) but a tolerance equal to 1e-06 is introduced.
 #' @param fx the integrand, the mathematical function to be integrated.
 #' @param lower the numerical lower bound of the integral.
 #' @param upper the numerical upper bound of the integral. 
@@ -17,9 +18,17 @@
 #'
 #' integrand <- function(x) {1/sqrt(2*pi)*exp(-x^2/2)} #this is the Normal distribution
 #' integPlot(fx=integrand,lower = -1.96,upper = 1.96)
+#' 
+#' integPlot(fx=function(x)1/4 * x^2,lower = 0,upper = 2)
+#' integPlot(fx=function(x)1/4 * x^2,lower = -Inf,upper = Inf)# indefinite integral
+#' integPlot(fx=function(x)1/4 * x^2,lower = -Inf,upper = 2) # improper integral: one of the bounds is infinity
+#' integPlot(fx=function(x)1/4 * x^2,lower = 0,upper = Inf)# improper integral 
 
 
 integPlot<-function(fx,lower,upper){
+  
+  if(lower==-Inf){lower<--1e-06}
+  if(upper==Inf){upper<-1e-06}
   
   x<-seq(lower,upper,by=0.1)
   
