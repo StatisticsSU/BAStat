6d2dd7c5334842097459c0f7883f761f276ade32
MaryAnne0725 <102033163+MaryAnne0725@users.noreply.github.com>
2022-08-03 11:04:24 +0200

    updating the asymmetric distributions

--- R/area for normal distribution and the t-student.R
+++ R/area for normal distribution and the t-student.R
@@ -1,66 +1,82 @@
-#' Draw the area under the standardized Normal density curve or the t-Student's curve. Both those functions are continous and symmetric, this is why the function is called "CS"
+#' Draw the area for continuous distributions: in this function is included the Standard Normal distribution and the CDF; the t-Student density, the chi-squared and F-Fisher density 
 #'
-#' @param cs by default is intended to draw the Standardized Normal distribution, otherwise specify *cs="t"* and it will for the t distribution
+#' @param distr by default is intended to draw the Standardized Normal distribution, otherwise specify *distr="t"* and it will be the t-distribution, but it has to be specified the degrees of freedom. 
+#' If *distr="chi.sq"* it is the chi-squared between 0 and 30 be drawn. Also in this case it has to be specified the number of degrees of freedom. If *distr="Fisher"* it will be density of the F-Fisher and in that case it would be needed to specify two degrees of freedom `deg.fred1` and `deg.fred2` 
 #' @param lower.x the lower bound of the symmetric and continuous distribution;
 #' @param upper.x the upper bound of the symmetric and continuous distribution;
 #' @param side it can be `left`, if the area is on the left side of the area; `right` if the area is on the right; `between` if the area is between two values
-#' @param deg.fred is the degree of freedom and it would be required if it is the t-Student (cs="t")
-#' @param cdf if it is true standard normal distribution is drawn with CDF
-#' @return the area under the standardized Normal distribution or the CDF for the normal or the area for the t-Student
+#' @param deg.fred is the degree of freedom and it would be required if it is the t-Student (distr="t") or chi-squared distribution (distr="chi.sq")
+#' @param cdf if it is true, then the CDF of a Standard Normal distribution will be drawn.
+#' @param value.x it is for one side chi-squared or F-Fisher distribution
+#' @param deg.fred1 and deg.fred2 have to be specified only for the Fisher
+#' @return the area under the standardized Normal distribution or the CDF for the normal or the area for the t-Student, chi-squared or F-Fisher density
 #' @export
 #' @examples
 #' library(BAStat)
 #' 
-#' CSareas(upper.x=1,side="left")#P(z<1)
-#' CSareas(upper.x=1,side="left",cdf=TRUE)#P(z<1)
-#' CSareas(upper.x=-1,side="left")#p(z<-1)
-#' CSareas(upper.x=-1,side="left",cdf=TRUE)#p(z<-1)
-#'
-#' CSareas(upper.x=-1,side="between")#wrong!
-#' CSareas(upper.x=-1,side="right")#wrong!
-#'
-#' CSareas(lower.x=-1,side="right")#P(z>-1)
-#' CSareas(lower.x=-1,side="right",cdf=TRUE)#F(-1)
-#' CSareas(lower.x=1,side="right")#P(z>1)
-#' CSareas(lower.x=1,side="right",cdf=TRUE)#F(1)
-
-#' CSareas(lower.x=1,upper.x=2,side="between")#P(1<Z<2)
-#' CSareas(lower.x=1,upper.x=2,side="between",cdf=TRUE)#F(2)-F(1)
-#' CSareas(lower.x=-1,upper.x=2,side="between")#P(-1<Z<2)
-#' CSareas(lower.x=-1,upper.x=2,side="between",cdf=TRUE)#F(2)-F(-1)
-#' CSareas(lower.x=-2,upper.x=-1,side="between")#P(-2<Z<-1)
-#' CSareas(lower.x=-2,upper.x=-1,side="between",cdf=TRUE)#F(-2)-F(-1)
+#' CdistrAreas(upper.x=1,side="left")#P(z<1)
+#' CdistrAreas(upper.x=-1,side="left")#p(z<-1)
 #' 
-#' CSareas(lower.x=2,upper.x=-1,side="between")#wrong!
-#' CSareas(lower.x=-1,upper.x=-2,side="between")#wrong!
-
-####
-#t-Student
-
-#' CSareas(cs="t",upper.x=2.49,deg.fred = 23,side="left")#P(t<2.49)
-#' CSareas(cs="t",upper.x=-2.49,deg.fred = 23,side="left")#p(t<-2.49)
+#' CdistrAreas(upper.x=1,side="left",cdf=T)#P(z<1)
+#' CdistrAreas(upper.x=-1,side="left",cdf=T)#p(z<-1)
 #' 
-#' CSareas(cs="t",upper.x=-2.49,deg.fred = 23,side="between")#wrong!
-#' CSareas(cs="t",upper.x=-2.49,deg.fred = 23,side="right")#wrong!
+#' CdistrAreas(upper.x=-1,side="between")#wrong!
+#' CdistrAreas(upper.x=-1,side="right")#wrong!
 #' 
-#' CSareas(cs="t",lower.x=-2.49,deg.fred = 23,side="right")#P(t>-2.49)
-#' CSareas(cs="t",lower.x=2.49,deg.fred = 23,side="right")#P(t>2.49)
-#' CSareas(cs="t",lower.x=-1.714,upper.x=1.714,deg.fred = 23,side="between") #P(-1.714<t<1.714)
-#' CSareas(cs="t",lower.x=-2.49,upper.x=3,deg.fred = 23,side="between") #P(-2.49<t<3)
-#' CSareas(cs="t",lower.x=-3,upper.x=-2.49,deg.fred = 23,side="between")#P(-3<t<-2.49)
-#'
-#' CSareas(cs="t",lower.x=2,upper.x=-1,deg.fred = 23,side="between")#wrong!
-#' CSareas(cs="t",lower.x=-1,upper.x=-2,deg.fred = 23,side="between")#wrong!
+#' CdistrAreas(lower.x=-1,side="right")#P(z>-1)
+#' CdistrAreas(lower.x=-1,side="right",cdf=T)#P(z>-1)
+#' CdistrAreas(lower.x=1,side="right")#P(z>1)
+#' CdistrAreas(lower.x=1,side="right",cdf=T)#P(z>1)
+#' 
+#' CdistrAreas(lower.x=1,upper.x=2,side="between")
+#' CdistrAreas(lower.x=-1,upper.x=2,side="between")
+#' CdistrAreas(lower.x=-2,upper.x=-1,side="between")
+#' 
+#' #cdf standard Normal distribution
+#' CdistrAreas(lower.x=1,upper.x=2,side="between",cdf=T)
+#' CdistrAreas(lower.x=-1,upper.x=2,side="between",cdf=T)
+#' CdistrAreas(lower.x=-2,upper.x=-1,side="between",cdf=T)
+#' 
+#' CdistrAreas(lower.x=2,upper.x=-1,side="between")#wrong!
+#' CdistrAreas(lower.x=-1,upper.x=-2,side="between")#wrong!
 
-#different areas with same bounds if it is normal or t-student we get -numerically-different areas
-#' CSareas(lower.x=1,upper.x=2,side="between")
-#' CSareas(cs="t",lower.x=1,upper.x=2,deg.fred=23,side="between")
+#' t-Student
+#' 
+#' CdistrAreas(distr="t",upper.x=2.49,deg.fred = 23,side="left")#P(t<2.49)
+#' CdistrAreas(distr="t",upper.x=-2.49,deg.fred = 23,side="left")#p(t<-2.49)
+#' CdistrAreas(distr="t",upper.x=-2.49,deg.fred = 23,side="between")#wrong!
+#' CdistrAreas(distr="t",upper.x=-2.49,deg.fred = 23,side="right")#wrong!
+#' 
+#' CdistrAreas(distr="t",lower.x=-2.49,deg.fred = 23,side="right")#P(t>-2.49)
+#' CdistrAreas(distr="t",lower.x=2.49,deg.fred = 23,side="right")#P(t>2.49)
+#' 
+#' 
+#' CdistrAreas(distr="t",lower.x=-1.714,upper.x=1.714,deg.fred = 23,side="between") #P(-1.714<t<1.714)
+#' CdistrAreas(distr="t",lower.x=-2.49,upper.x=3,deg.fred = 23,side="between") #P(-2.49<t<3)
+#' CdistrAreas(distr="t",lower.x=-3,upper.x=-2.49,deg.fred = 23,side="between")#P(-3<t<-2.49)
+#' 
+#' CdistrAreas(distr="t",lower.x=2,upper.x=-1,deg.fred = 23,side="between")#wrong!
+#' CdistrAreas(distr="t",lower.x=-1,upper.x=-2,deg.fred = 23,side="between")#wrong!
+#' 
+#' #different areas with same bounds if it is normal or t-student we get -numerically-different areas
+#' CdistrAreas(lower.x=1,upper.x=2,side="between")
+#' CdistrAreas(distr="t",lower.x=1,upper.x=2,deg.fred=23,side="between")
+#' 
+#' ###chi.squared
+#' CdistrAreas(distr="chi.sq",value.x =11,deg.fred=5,side="left")
+#' CdistrAreas(distr="chi.sq",value.x=11,deg.fred=5,side="right")
+#' CdistrAreas(distr="chi.sq",lower.x=11,upper.x=12,deg.fred=5,side="between")
+#' 
+#' ###Fisher
+#' CdistrAreas(distr="Fisher",value.x =2,deg.fred1=5,deg.fred2=10,side="left")
+#' CdistrAreas(distr="Fisher",value.x=2.5,deg.fred1=5,deg.fred2=10,side="right")
+#' CdistrAreas(distr="Fisher",lower.x=2,upper.x=3,deg.fred1=5,deg.fred2=10,side="between")
 
 
-
-CSareas<-function(cs="Standard Normal",lower.x=NULL, upper.x=NULL, side,deg.fred, cdf=FALSE){
+CdistrAreas<-function(distr="Standard Normal",lower.x=NULL, upper.x=NULL, side,deg.fred, value.x=NULL,deg.fred1,deg.fred2,cdf=FALSE){
   
-  if(cs=="Standard Normal"){
+  
+  if(distr=="Standard Normal"){
     Grid= seq(-4,4,by = 0.01)
     plot(Grid, dnorm(Grid), type = "l", xlab = "z", ylab = "Standardized Normal density",lwd=1,xaxt="n")
     axis(1,seq(-4,4,by=1))
@@ -82,8 +98,8 @@ CSareas<-function(cs="Standard Normal",lower.x=NULL, upper.x=NULL, side,deg.fred
     
     
     if(side=="right"){
-      upper.x<-4
       if((lower.x>-4)&(lower.x>=0)){
+        upper.x<-4
         mtext(paste("area:", round(1-pnorm(lower.x),4)), side = 3)
         x = seq(from = lower.x, to = max(Grid), length.out = 1000) 
         y = normPlot(x)
@@ -91,23 +107,19 @@ CSareas<-function(cs="Standard Normal",lower.x=NULL, upper.x=NULL, side,deg.fred
         x = c(lower.x, max(Grid), x, lower.x) 
         y = c(0,0,0, y)
         polygon(x,y, lty = 3, border = NULL, col = "Light blue")
-        
       }
     }
     
     if(side=="left"){
-      lower.x<--4
       if((upper.x<4)&(upper.x<0)){
-        
+        lower.x<--4 
         x1 = seq(from = min(Grid), to = upper.x, length.out = 1000) 
         y1 = normPlot(x1)
         x1 = c(upper.x,min(Grid), x1, upper.x) 
-        
         y1 = c(y1,0, 0, 0)
         polygon(x1,y1, lty = 3, border = NULL, col = "Light blue")
         
         mtext(paste("area:", round(pnorm(upper.x),4)), side = 3)
-        
       }
     }
     
@@ -122,8 +134,6 @@ CSareas<-function(cs="Standard Normal",lower.x=NULL, upper.x=NULL, side,deg.fred
         x = c(upper.x, min(Grid), x, upper.x) 
         y = c(0, y, 0, 0)
         polygon(x,y, lty = 3, border = NULL, col = "Light blue")
-        
-        
       }
     }
     
@@ -132,7 +142,6 @@ CSareas<-function(cs="Standard Normal",lower.x=NULL, upper.x=NULL, side,deg.fred
       if((lower.x>=0) & (upper.x>=0)){
         
         mtext(paste("area:", round((pnorm(upper.x)-pnorm(lower.x)),4)), side = 3)
-        
         x = seq(from = min(Grid), to = upper.x, length.out = 1000) 
         y = normPlot(x)
         
@@ -147,7 +156,6 @@ CSareas<-function(cs="Standard Normal",lower.x=NULL, upper.x=NULL, side,deg.fred
         x1 = c(lower.x, min(Grid), x1, lower.x) 
         y1 = c(0, y1, 0, 0)
         polygon(x1,y1, lty = 3, border = NULL, col = "White")
-        
       }
       
       if((lower.x<0) & (upper.x>=0)){
@@ -169,7 +177,6 @@ CSareas<-function(cs="Standard Normal",lower.x=NULL, upper.x=NULL, side,deg.fred
         y1 = c(0, y1, 0, 0)
         polygon(x1,y1, lty = 3, border = NULL, col = "White")
         
-        
       }
       
       
@@ -185,7 +192,6 @@ CSareas<-function(cs="Standard Normal",lower.x=NULL, upper.x=NULL, side,deg.fred
         
         x1 = seq(from = min(Grid), to = lower.x, length.out = 1000) 
         y1 = normPlot(x1)
-        
         x1 = c(lower.x, min(Grid), x1, lower.x) 
         y1 = c(0, y1, 0, 0)
         polygon(x1,y1, lty = 3, border = NULL, col = "White")
@@ -231,9 +237,8 @@ CSareas<-function(cs="Standard Normal",lower.x=NULL, upper.x=NULL, side,deg.fred
       }
       
       if(side=="right"){
-        upper.x<-4
         if((lower.x>-4)&(lower.x>=0)){
-          
+          upper.x<-4
           mtext(paste("area:", round(1-pnorm(lower.x),4)), side = 3)
           
           p <- c(upper.x, pnorm(upper.x))#pnorm(1, mean = 0 , sd=1)
@@ -259,7 +264,6 @@ CSareas<-function(cs="Standard Normal",lower.x=NULL, upper.x=NULL, side,deg.fred
       }
       
       if(side=="left"){
-        
         if((upper.x<4)&(upper.x<0)){
           lower.x<--4
           mtext(paste("area:", round(pnorm(upper.x),4)), side = 3)
@@ -342,7 +346,6 @@ CSareas<-function(cs="Standard Normal",lower.x=NULL, upper.x=NULL, side,deg.fred
         }
         
         if((lower.x<0) & (upper.x>=0)){
-          
           mtext(paste("area:", round((pnorm(upper.x)-pnorm(lower.x)),4)), side = 3)
           
           p <- c(upper.x, pnorm(upper.x))
@@ -395,22 +398,15 @@ CSareas<-function(cs="Standard Normal",lower.x=NULL, upper.x=NULL, side,deg.fred
     } 
     
     
-    
-    
-    
-    
   }
+  #### 
   
-  
-  if(cs=="t"){
-    
-    
+  if(distr=="t"){
     
     Grid= seq(-4,4,by = 0.01)
-    
     plot(Grid, dt(Grid,df=deg.fred), type = "l", xlab = "t", ylab = "t-Student density",lwd=1,xaxt="n")
     axis(1,seq(-4,4,by=1))
-    normPlot = function(x,df){dt(x,df=deg.fred)}  
+    tPlot = function(x,df){dt(x,df=deg.fred)}  
     
     if(side=="right"){
       if((lower.x>-4)&(lower.x<0)){
@@ -418,7 +414,7 @@ CSareas<-function(cs="Standard Normal",lower.x=NULL, upper.x=NULL, side,deg.fred
         mtext(paste("area:", round(1-pt(lower.x,df=deg.fred),4)), side = 3)
         
         x = seq(from = lower.x, to = max(Grid), length.out = 1000) 
-        y = normPlot(x)
+        y = tPlot(x,df=deg.fred)
         
         x = c(lower.x, max(Grid), x, lower.x) 
         y = c(0,0,0, y)
@@ -428,11 +424,12 @@ CSareas<-function(cs="Standard Normal",lower.x=NULL, upper.x=NULL, side,deg.fred
     
     
     if(side=="right"){
-      upper.x<-4
+      
       if((lower.x>-4)&(lower.x>=0)){
+        upper.x<-4
         mtext(paste("area:", round(1-pt(lower.x,df=deg.fred),4)), side = 3)
         x = seq(from = lower.x, to = max(Grid), length.out = 1000) 
-        y = normPlot(x)
+        y = tPlot(x,df=deg.fred)
         
         x = c(lower.x, max(Grid), x, lower.x) 
         y = c(0,0,0, y)
@@ -446,7 +443,7 @@ CSareas<-function(cs="Standard Normal",lower.x=NULL, upper.x=NULL, side,deg.fred
       if((upper.x<4)&(upper.x<0)){
         
         x1 = seq(from = min(Grid), to = upper.x, length.out = 1000) 
-        y1 = normPlot(x1)
+        y1 = tPlot(x1,df=deg.fred)
         x1 = c(upper.x,min(Grid), x1, upper.x) 
         
         y1 = c(y1,0, 0, 0)
@@ -463,7 +460,7 @@ CSareas<-function(cs="Standard Normal",lower.x=NULL, upper.x=NULL, side,deg.fred
         mtext(paste("area:", round(pt(upper.x,df=deg.fred),4)), side = 3)
         
         x = seq(from = min(Grid), to = upper.x, length.out = 1000) 
-        y = normPlot(x)
+        y = tPlot(x,df=deg.fred)
         
         x = c(upper.x, min(Grid), x, upper.x) 
         y = c(0, y, 0, 0)
@@ -480,7 +477,7 @@ CSareas<-function(cs="Standard Normal",lower.x=NULL, upper.x=NULL, side,deg.fred
         mtext(paste("area:", round((pt(upper.x,df=deg.fred)-pt(lower.x,df=deg.fred)),4)), side = 3)
         
         x = seq(from = min(Grid), to = upper.x, length.out = 1000) 
-        y = normPlot(x)
+        y = tPlot(x,df=deg.fred)
         
         x = c(upper.x, min(Grid), x, upper.x) 
         y = c(0, y, 0, 0)
@@ -488,7 +485,7 @@ CSareas<-function(cs="Standard Normal",lower.x=NULL, upper.x=NULL, side,deg.fred
         
         
         x1 = seq(from = min(Grid), to = lower.x, length.out = 1000) 
-        y1 = normPlot(x1)
+        y1 = tPlot(x1,df=deg.fred)
         
         x1 = c(lower.x, min(Grid), x1, lower.x) 
         y1 = c(0, y1, 0, 0)
@@ -501,7 +498,7 @@ CSareas<-function(cs="Standard Normal",lower.x=NULL, upper.x=NULL, side,deg.fred
         mtext(paste("area:", round((pt(upper.x,df=deg.fred)-pt(lower.x,df=deg.fred)),4)), side = 3)
         
         x = seq(from = min(Grid), to = upper.x, length.out = 1000) 
-        y = normPlot(x)
+        y = tPlot(x,df=deg.fred)
         
         x = c(upper.x, min(Grid), x, upper.x) 
         y = c(0, y, 0, 0)
@@ -509,14 +506,11 @@ CSareas<-function(cs="Standard Normal",lower.x=NULL, upper.x=NULL, side,deg.fred
         
         
         x1 = seq(from = min(Grid), to = lower.x, length.out = 1000) 
-        y1 = normPlot(x1)
+        y1 = tPlot(x1,df=deg.fred)
         
         x1 = c(lower.x, min(Grid), x1, lower.x) 
         y1 = c(0, y1, 0, 0)
         polygon(x1,y1, lty = 3, border = NULL, col = "White")
-        
-        
-        
         
       }
       
@@ -526,7 +520,7 @@ CSareas<-function(cs="Standard Normal",lower.x=NULL, upper.x=NULL, side,deg.fred
         mtext(paste("area:", round((pt(upper.x,df=deg.fred)-pt(lower.x,df=deg.fred)),4)), side = 3)
         
         x = seq(from = min(Grid), to = upper.x, length.out = 1000) 
-        y = normPlot(x)
+        y = tPlot(x,df=deg.fred)
         
         x = c(upper.x, min(Grid), x, upper.x) 
         y = c(0, y, 0, 0)
@@ -534,17 +528,148 @@ CSareas<-function(cs="Standard Normal",lower.x=NULL, upper.x=NULL, side,deg.fred
         
         
         x1 = seq(from = min(Grid), to = lower.x, length.out = 1000) 
-        y1 = normPlot(x1)
+        y1 = tPlot(x1,df=deg.fred)
         
         x1 = c(lower.x, min(Grid), x1, lower.x) 
         y1 = c(0, y1, 0, 0)
         polygon(x1,y1, lty = 3, border = NULL, col = "White")
         
       }
+      
+      
+    }
+    
+    
+  }
+  #####
+  
+  
+  if(distr=="chi.sq"){
+    
+    Grid= seq(0,30,by = 0.01)
+    plot(Grid, dchisq(Grid,df=deg.fred), type = "l", xlab = expression(chi^2), ylab = "Chi-squared density",lwd=1,xaxt="n")
+    axis(1,seq(0,30,by=1))
+    chiPlot = function(x,df){dchisq(x,df=deg.fred)}  
+    
+    if(side=="right"){
+      
+      upper.x<-30
+      mtext(paste("area:", round(1-pchisq(value.x,df=deg.fred),4)), side = 3)
+      
+      x = seq(from = value.x, to = max(Grid), length.out = 1000) 
+      y = chiPlot(x,df=deg.fred)
+      
+      x = c(value.x, max(Grid), x, value.x) 
+      y = c(0,0,0, y)
+      polygon(x,y, lty = 3, border = NULL, col = "Light blue")
+      
+    }
+    
+    
+    if(side=="left"){
+      lower.x<-0
+      
+      x1 = seq(from = min(Grid), to = value.x, length.out = 1000) 
+      y1 = chiPlot(x1,df=deg.fred)
+      x1 = c(value.x,min(Grid), x1, value.x) 
+      
+      y1 = c(y1,0, 0, 0)
+      polygon(x1,y1, lty = 3, border = NULL, col = "Light blue")
+      
+      mtext(paste("area:", round(pchisq(value.x,df=deg.fred),4)), side = 3)
+      
+    }
+    
+    
+    
+    if(side=="between"){
+      if((lower.x>=0) & (upper.x>=0)){
+        
+        mtext(paste("area:", round((pchisq(upper.x,df=deg.fred)-pchisq(lower.x,df=deg.fred)),4)), side = 3)
+        x = seq(from = min(Grid), to = upper.x, length.out = 1000) 
+        y = chiPlot(x,df=deg.fred)
+        x = c(upper.x, min(Grid), x, upper.x) 
+        y = c(0, y, 0, 0)
+        polygon(x,y, lty = 3, border = NULL, col = "Light blue")
+        
+        
+        x1 = seq(from = min(Grid), to = lower.x, length.out = 1000) 
+        y1 = chiPlot(x1,df=deg.fred)
+        x1 = c(lower.x, min(Grid), x1, lower.x) 
+        y1 = c(0, y1, 0, 0)
+        polygon(x1,y1, lty = 3, border = NULL, col = "White")
+        
+      }
+      
+    }
+    
+  }#chisq
+  
+  
+  ####Fisher
+  
+  if(distr=="Fisher"){
+    
+    Grid= seq(0,30,by = 0.01)
+    plot(Grid, df(Grid,df1=deg.fred1,df2=deg.fred2), type = "l", xlab = "F", ylab = "Fisher density",lwd=1,xaxt="n")
+    axis(1,seq(0,30,by=1))
+    FPlot = function(x,df1,df2){df(x,df1=deg.fred1,df2=deg.fred2)}  
+    
+    if(side=="right"){
+      
+      upper.x<-30
+      mtext(paste("area:", round(1-pf(value.x,df1=deg.fred1,df2=deg.fred2),4)), side = 3)
+      
+      x = seq(from = value.x, to = max(Grid), length.out = 1000) 
+      y = FPlot(x,df1=deg.fred1,df2=deg.fred2)
+      
+      x = c(value.x, max(Grid), x, value.x) 
+      y = c(0,0,0, y)
+      polygon(x,y, lty = 3, border = NULL, col = "Light blue")
+      
+    }
+    
+    # }
+    
+    if(side=="left"){
+      lower.x<-0
+      
+      x1 = seq(from = min(Grid), to = value.x, length.out = 1000) 
+      y1 = FPlot(x1,df1=deg.fred1,df2=deg.fred2)
+      x1 = c(value.x,min(Grid), x1, value.x) 
+      
+      y1 = c(y1,0, 0, 0)
+      polygon(x1,y1, lty = 3, border = NULL, col = "Light blue")
+      
+      mtext(paste("area:", round(pf(value.x,df1=deg.fred1,df2=deg.fred2),4)), side = 3)
+      
+    }
+    
+    
+    
+    
+    if(side=="between"){
+      if((lower.x>=0) & (upper.x>=0)){
+        
+        mtext(paste("area:", round((pf(upper.x,df1=deg.fred1,df2=deg.fred2)-pf(lower.x,df1=deg.fred1,df2=deg.fred2)),4)), side = 3)
+        x = seq(from = min(Grid), to = upper.x, length.out = 1000) 
+        y = FPlot(x,df1=deg.fred1,df2=deg.fred2)
+        x = c(upper.x, min(Grid), x, upper.x) 
+        y = c(0, y, 0, 0)
+        polygon(x,y, lty = 3, border = NULL, col = "Light blue")
+        
+        
+        x1 = seq(from = min(Grid), to = lower.x, length.out = 1000) 
+        y1 = FPlot(x1,df1=deg.fred1,df2=deg.fred2)
+        x1 = c(lower.x, min(Grid), x1, lower.x) 
+        y1 = c(0, y1, 0, 0)
+        polygon(x1,y1, lty = 3, border = NULL, col = "White")
+        
+      }
+      
     }
     
   }
-  
   
   
 }
